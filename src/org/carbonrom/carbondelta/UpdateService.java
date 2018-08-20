4ddbe1932715426fa1587c0aebf3c1751ea12641
maxwen <max.weninger@gmail.com>
2018-08-20 16:59:23 +0200

    OpenDelta: find the white mouse
    last usages on 8.1 showed that old images have not been deleted
    as they should.
    
    Change-Id: I6e23f963e719cce02c4671949fe08d0667f068da

--- src/org/carbonrom/carbondelta/UpdateService.java
+++ src/org/carbonrom/carbondelta/UpdateService.java
@@ -186,6 +186,8 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     // we only snooze until a new build
     private static final String PREF_SNOOZE_UPDATE_NAME = "last_snooze_update";
 
+    public static final String PREF_CURRENT_FILENAME_NAME = "current_filename";
+
     private static final long SNOOZE_MS = 24 * AlarmManager.INTERVAL_HOUR;
 
     public static final String PREF_AUTO_UPDATE_NETWORKS_NAME = "auto_update_networks";
@@ -228,7 +230,6 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     private boolean updateRunning;
     private int failedUpdateCount;
     private SharedPreferences prefs = null;
-    private String oldFlashFilename;
     private Notification.Builder mBuilder;
     private boolean isProgressNotificationDismissed = false;
     private long progressUpdateStart;
@@ -1443,7 +1444,6 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
 
         if (downloadUrlFileUnknownSize(url, f, md5Sum)) {
             Logger.d("success");
-            deleteOldFlashFile(fn);
             prefs.edit().putString(PREF_READY_FILENAME_NAME, fn).commit();
         } else {
             f.delete();
@@ -1580,6 +1580,9 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     protected void onUpdateCompleted(int status) {
         stopNotification();
         if (status == UpdateEngine.ErrorCodeConstants.SUCCESS) {
+            String flashFilename = prefs.getString(PREF_READY_FILENAME_NAME, PREF_READY_FILENAME_DEFAULT);
+            deleteOldFlashFile(flashFilename);
+            prefs.edit().putString(PREF_CURRENT_FILENAME_NAME, flashFilename).commit();
             startABRebootNotification();
             updateState(STATE_ACTION_AB_FINISHED, null, null, null, null, null);
         } else {
@@ -1698,7 +1701,8 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
             Logger.ex(ex);
         }
 
-        clearState();
+        deleteOldFlashFile(flashFilename);
+        prefs.edit().putString(PREF_CURRENT_FILENAME_NAME, flashFilename).commit();
 
         // Remove the path to the storage from the filename, so we get a path
         // relative to the root of the storage
@@ -1978,7 +1982,6 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     private void clearState() {
         prefs.edit().putString(PREF_LATEST_FULL_NAME, PREF_READY_FILENAME_DEFAULT).commit();
         prefs.edit().putString(PREF_LATEST_DELTA_NAME, PREF_READY_FILENAME_DEFAULT).commit();
-        oldFlashFilename = prefs.getString(PREF_READY_FILENAME_NAME, PREF_READY_FILENAME_DEFAULT);
         prefs.edit().putString(PREF_READY_FILENAME_NAME, PREF_READY_FILENAME_DEFAULT).commit();
         prefs.edit().putLong(PREF_DOWNLOAD_SIZE, -1).commit();
         prefs.edit().putBoolean(PREF_DELTA_SIGNATURE, false).commit();
@@ -2000,6 +2003,8 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     }
 
     private void checkForUpdatesAsync(final boolean userInitiated, final int checkOnly) {
+        Logger.d("checkForUpdatesAsync " + getPrefs().getAll());
+
         updateState(STATE_ACTION_CHECKING, null, null, null, null, null);
         wakeLock.acquire();
         wifiLock.acquire();
@@ -2240,7 +2245,6 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
                             if (new File(fn).exists()) {
                                 if (checkFullBuildMd5Sum(latestFullFetch, fn)) {
                                     Logger.d("match found (full): " + fn);
-                                    deleteOldFlashFile(fn);
                                     prefs.edit().putString(PREF_READY_FILENAME_NAME, fn).commit();
                                     downloadFullBuild = false;
                                 } else {
@@ -2312,7 +2316,6 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
                                 }
                             }
                             prefs.edit().putBoolean(PREF_DELTA_SIGNATURE, true).commit();
-                            deleteOldFlashFile(flashFilename);
                             prefs.edit().putString(PREF_READY_FILENAME_NAME, flashFilename).commit();
                         }
                     }
@@ -2361,6 +2364,9 @@ OnWantUpdateCheckListener, OnSharedPreferenceChangeListener {
     }
 
     private void deleteOldFlashFile(String newFlashFilename) {
+        String oldFlashFilename = prefs.getString(PREF_CURRENT_FILENAME_NAME, PREF_READY_FILENAME_DEFAULT);
+        Logger.d("delete oldFlashFilename " + oldFlashFilename + " " + newFlashFilename);
+
         if (oldFlashFilename != PREF_READY_FILENAME_DEFAULT && !oldFlashFilename.equals(newFlashFilename)
                 && oldFlashFilename.startsWith(config.getPathBase())) {
             File file = new File(oldFlashFilename);
